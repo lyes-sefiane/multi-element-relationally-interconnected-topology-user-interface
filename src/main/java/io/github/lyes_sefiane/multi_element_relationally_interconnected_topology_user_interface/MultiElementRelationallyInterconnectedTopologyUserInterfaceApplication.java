d09b76bd7a3ddf30fcc6bd478ad854f12ceaa74a
lyes-s <lyes.sefiane@gmail.com>
2024-12-13 14:44:25 -0500

    first commit

---
+++ src/main/java/io/github/lyes_sefiane/multi_element_relationally_interconnected_topology_user_interface/MultiElementRelationallyInterconnectedTopologyUserInterfaceApplication.java
@@ -0,0 +1,13 @@
+package io.github.lyes_sefiane.multi_element_relationally_interconnected_topology_user_interface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiElementRelationallyInterconnectedTopologyUserInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiElementRelationallyInterconnectedTopologyUserInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lyes_sefiane/multi_element_relationally_interconnected_topology_user_interface/MultiElementRelationallyInterconnectedTopologyUserInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lyes_sefiane.multi_element_relationally_interconnected_topology_user_interface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiElementRelationallyInterconnectedTopologyUserInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
